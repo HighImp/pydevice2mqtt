c63c17e67a6f6204033be75a28661fad199b4c89
HighImp <Seb.Koell@googlemail.com>
2023-01-28 16:05:29 +0100

    Add Support for Arbitrary Sensor Add config requirement dict for all devices

--- remote_devices.py
+++ remote_devices.py
@@ -1,6 +1,8 @@
 import json
+import logging
 import time
 from collections import namedtuple, defaultdict
+from abc import abstractmethod, ABC
 
 # special_modules = list()
 # special_modules.append("subprocess")
@@ -15,12 +17,12 @@ from collections import namedtuple, defaultdict
 MQTTChannel = namedtuple("MQTTChannel", ["topic", "on_message"])
 
 
-class RemoteDevice:
+class RemoteDevice(ABC):
 
-    def __init__(self, device_settings, mqtt_settings):
+    def __init__(self, device_settings: dict, mqtt_settings: dict):
         self._operation_topics = defaultdict(MQTTChannel)
         self._discovery_prefix = f"{mqtt_settings['discovery_prefix']}/" \
-                                 f"{device_settings['component']}/" \
+                                 f"{device_settings['device_class']}/" \
                                  f"{mqtt_settings['node_id']}/" \
                                  f"{device_settings['object_id']}/"
 
@@ -41,16 +43,19 @@ class RemoteDevice:
                           sub_topic="state",
                           on_message=None)
 
-        self._component = device_settings['component']
+        self._device_class = device_settings['device_class']
         self._config["name"] = device_settings["name"]
 
         self._config["unique_id"] = device_settings["uid"]
+
+        # connect self._publish to the function publish
         self._publish = mqtt_settings["f_publish"]
 
-    def _print_log(self, *args, **kwargs):
+    def _log_remote(self, *args, **kwargs):
 
+        level = getattr(kwargs, "Level", logging.DEBUG)
         print_string = "".join(map(str, args))
-        print(print_string, **kwargs)
+        logging.log(level=level, msg=print_string)
         if self._logging_channel is not None:
             self._publish(topic=self._logging_channel,
                           payload=print_string,
@@ -92,9 +97,9 @@ class RemoteDevice:
 
     def get_name(self) -> str:
         """
-        Get the given of the component, defined in config
+        Get the given name of the device, defined in config
 
-        :return: name of the component
+        :return: name of the device
         """
         return self._config["name"]
 
@@ -119,8 +124,15 @@ class RemoteDevice:
         """
         return dict([channel for name, channel in self._operation_topics.items()])
 
+    @classmethod
+    @abstractmethod
+    def get_config_req(cls) -> dict:
+        """Returns a dict with the required keys and the expected data types as values
+        :return: dict
+        """
 
-def supported_devices() -> dict:
+
+def supported_device_classes() -> dict:
     """
     generates a dictionary with all supported devices,
     and the class name as key, the object as value
@@ -134,12 +146,69 @@ def supported_devices() -> dict:
     return dict((name, obj) for name, obj in cls_members if name != "RemoteDevice")
 
 
+class ArbitrarySensor(RemoteDevice):
+    """
+    Arbitrary Sensor to publish any data to hassio
+    """
+
+    _CONFIG_REQ = {
+        "name": str,  # Display Name
+        "device_class": str,  # Sensor Type (https://www.home-assistant.io/integrations/sensor#device-class)
+        "unit_of_measurement": str,  # Unit of measurement (W,C,V,A...)
+        "object_id": str  # Unique Object ID, will determine config and state topic
+    }
+
+    def __init__(self, device_settings: dict, mqtt_settings: dict):
+        # mqtt discovery channel must be start with sensor, but
+        # but the final device class will be correct
+        # -> Discovery channel will be created in constructor of parent class
+        org_device_class = device_settings["device_class"]
+        device_settings["device_class"] = "sensor"
+        super(ArbitrarySensor, self).__init__(device_settings=device_settings, mqtt_settings=mqtt_settings)
+        device_settings["device_class"] = org_device_class
+        self._config["device_class"] = device_settings.get("device_class", "None")
+        self._config["unit_of_measurement"] = device_settings.get("unit_of_measurement", "")
+        self._config["value_template"] = "{{ value_json.value}}"
+        self._last_value = None
+
+    def set_value(self, value, force_update=True) -> None:
+        """The set function for this sensor,
+        will update the value in hassio.
+
+        :param value: new arbitrary value
+        :param force_update: if true, write the state even if no value changes occured
+
+        :return:
+        """
+        if self._last_value == value and not force_update:
+            return
+
+        self._update(channel_name="state_topic",
+                     message=json.dumps({"value": value}))
+        self._last_value = value
+
+    @classmethod
+    def get_config_req(cls) -> dict:
+        """Returns a dict with the required keys and the expected data types as values
+        :return: dict
+        """
+        return cls._CONFIG_REQ
+
+
 class RPI_GPIO(RemoteDevice):
     """
     Raspberry PI Remote Gpio device
     Remote device to configure and control GPIOs of an Raspberry Pi via MQTT and
     especially in hassio via auto configuration, supporting switch and binary sensor format
     """
+
+    _CONFIG_REQ = {
+        "name": str,  # Display Name
+        "device_class": str,  # binary_sensor or switch
+        "object_id": str,  # Unique Object ID, will determine config and state topic
+        "pin": int,  # Pin Nr according to gpiozero
+        "inverted": bool  # Device side inverter on/off (both directions)
+    }
 
     def __init__(self, device_settings, mqtt_settings):
         super(RPI_GPIO, self).__init__(device_settings=device_settings, mqtt_settings=mqtt_settings)
@@ -148,18 +217,18 @@ class RPI_GPIO(RemoteDevice):
             import gpiozero
         except ImportError:
             err_msg = "Could not import gpiozero. Unable to create this remote device!"
-            self._print_log(err_msg)
+            self._log_remote(err_msg)
             raise ImportError(err_msg)
 
         self._gpiozero_device = None
         self._inverted = device_settings["inverted"]
-        if device_settings["component"] == "binary_sensor":
+        if device_settings["device_class"] == "binary_sensor":
             self._gpiozero_device = gpiozero.DigitalInputDevice(pin=device_settings["pin"])
 
             self._gpiozero_device.when_activated = lambda msg="ON": self._handle_pinchange(msg)
             self._gpiozero_device.when_deactivated = lambda msg="OFF": self._handle_pinchange(msg)
 
-        elif device_settings["component"] == "switch":
+        elif device_settings["device_class"] == "switch":
             self._add_channel(channel_name="command_topic",
                               sub_topic="set",
                               on_message=self._handle_command)
@@ -186,8 +255,25 @@ class RPI_GPIO(RemoteDevice):
         self._update(channel_name="state_topic",
                      message=target_state)
 
+    @classmethod
+    def get_config_req(cls) -> dict:
+        """Returns a dict with the required keys and the expected data types as values
+        :return: dict
+        """
+        return cls._CONFIG_REQ
+
 
 class RPI_RGB(RemoteDevice):
+    _CONFIG_REQ = {
+        "name": str,  # Display Name
+        "device_class": str,  # binary_sensor or switch
+        "object_id": str,  # Unique Object ID, will determine config and state topic
+        "pin_r": int,  # Red Pin Nr according to gpiozero
+        "pin_g": int,  # Green Pin Nr according to gpiozero
+        "pin_b": int,  # Blue Pin Nr according to gpiozero
+        "active_high": bool,  # Determine if logical 1 leads to active light
+        "pwm_led": bool  # Determine if the pins support PWM
+    }
 
     def __init__(self, device_settings, mqtt_settings):
 
@@ -197,7 +283,7 @@ class RPI_RGB(RemoteDevice):
             import gpiozero
         except ImportError:
             err_msg = "Could not import gpiozero. Unable to create this remote device!"
-            self._print_log(err_msg)
+            self._log_remote(err_msg)
             raise ImportError(err_msg)
 
         self._config["schema"] = "json"
@@ -243,7 +329,7 @@ class RPI_RGB(RemoteDevice):
 
 
         except KeyError:
-            self._print_log(f"Unsupported command structure: {message}")
+            self._log_remote(f"Unsupported command structure: {message}")
 
     def _publish_state(self, rgb):
 
@@ -254,8 +340,24 @@ class RPI_RGB(RemoteDevice):
         self._update(channel_name="rgb_state_topic",
                      message=message)
 
+    @classmethod
+    def get_config_req(cls) -> dict:
+        """Returns a dict with the required keys and the expected data types as values
+        :return: dict
+        """
+        return cls._CONFIG_REQ
+
 
 class ESpeakTTS(RemoteDevice):
+    _CONFIG_REQ = {
+        "name": str,  # Display Name
+        "device_class": str,  # Should always be tts
+        "object_id": str,  # Unique Object ID, will determine config and state topic
+        "voice": str,  # ESpeak voice set (like 'mb-de6')
+        "rate": int,  # Voice Speed
+        "pitch": int,  # Voice Pitch
+    }
+
     def __init__(self, device_settings, mqtt_settings):
         super(ESpeakTTS, self).__init__(device_settings=device_settings, mqtt_settings=mqtt_settings)
 
@@ -263,7 +365,7 @@ class ESpeakTTS(RemoteDevice):
             self._espeak_module = __import__("espeak", fromlist=["espeak"])
         except ImportError:
             err_msg = "Could not import espeak. Unable to create this remote device!"
-            self._print_log(err_msg)
+            self._log_remote(err_msg)
             raise ImportError(err_msg)
 
         self._config["schema"] = "json"
@@ -288,7 +390,14 @@ class ESpeakTTS(RemoteDevice):
                 text = str(msg["text"])
             self._espeak_module.espeak.synth(text)
         except (KeyError, TypeError):
-            self._print_log("No text key provided in message!")
+            self._log_remote("No text key provided in message!")
+
+    @classmethod
+    def get_config_req(cls) -> dict:
+        """Returns a dict with the required keys and the expected data types as values
+        :return: dict
+        """
+        return cls._CONFIG_REQ
 
 
 class SubprocessCall(RemoteDevice):
@@ -296,9 +405,19 @@ class SubprocessCall(RemoteDevice):
     Remote Subprocess call via MQTT
     the call is defined in the config
 
-    Will be treated as an switch with: "on" start, and "off" killing
-    an ongoing subprocess. The state channel will publish "on" during an ongoing process
+    Will be treated as a switch.
+    "on" will start, and "off" killing a ongoing subprocess.
+    The state channel will publish "on" during active process
     """
+
+    _CONFIG_REQ = {
+        "name": str,  # Display Name
+        "device_class": str,  # should be 'switch'
+        "object_id": str,  # Unique Object ID, will determine config and state topic
+        "exec_path": str,  # Path app or file to execute (i.E. python.exe)
+        "arguments": str,  # space separated list of arguments ("--version -E")
+        "looptime": int  # polling time in seconds to check if the call is done
+    }
 
     def __init__(self, device_settings, mqtt_settings):
         super(SubprocessCall, self).__init__(device_settings=device_settings, mqtt_settings=mqtt_settings)
@@ -308,14 +427,14 @@ class SubprocessCall(RemoteDevice):
             self._subprocess_module = __import__("subprocess", fromlist=["subprocess"])
         except ImportError:
             err_msg = "Could not import threading and/or subprocess. Unable to create this remote device!"
-            self._print_log(err_msg)
+            self._log_remote(err_msg)
             raise ImportError(err_msg)
 
         self._call = [device_settings["exec_path"]]
         try:
             args = device_settings["arguments"]
             if isinstance(args, dict):
-                self._call.extend([param+" "+args[param] for param in args])
+                self._call.extend([param + " " + args[param] for param in args])
             if isinstance(args, list):
                 self._call.extend(args)
             if isinstance(args, str):
@@ -324,8 +443,8 @@ class SubprocessCall(RemoteDevice):
         except KeyError:
             pass
 
-        self._print_log("Device created: SubprocessCall is: ", self._call)
-        
+        self._log_remote("Device created: SubprocessCall is: ", self._call)
+
         try:
             self._looptime = device_settings["looptime"]
         except KeyError:
@@ -361,13 +480,13 @@ class SubprocessCall(RemoteDevice):
         if target_state == "ON":
 
             if self._running_process is not None:
-                self._print_log("Error: Subprocess is still running, multirun not supported")
+                self._log_remote("Error: Subprocess is still running, multirun not supported")
                 return
 
             try:
                 self._running_process = self._subprocess_module.Popen(self._call)
             except Exception as error:
-                self._print_log("{}".format(error))
+                self._log_remote("{}".format(error))
             else:
                 thread_args = {"pOpen": self._running_process, "looptime": self._looptime}
                 self._observation_thread = self._threading_module.Thread(target=self._observation_function,
@@ -377,7 +496,14 @@ class SubprocessCall(RemoteDevice):
 
         if target_state == "OFF":
             if self._running_process is None:
-                self._print_log("Process already stopped")
+                self._log_remote("Process already stopped")
                 return
 
             self._running_process.kill()
+
+    @classmethod
+    def get_config_req(cls) -> dict:
+        """Returns a dict with the required keys and the expected data types as values
+        :return: dict
+        """
+        return cls._CONFIG_REQ
